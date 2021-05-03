682e1d71ff6442d152afec9ede596afda6d174f2
Maisie <maisieobb@googlemail.com>
2021-05-03 18:45:28 +0100

    added in pictures of projects

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,23 +1,24 @@
 import React from "react";
-
+import walkimg from "./WhenShouldIWalk.JPG"
+import workwithme from "./WorkWithME.JPG"
 export default function Portfolio() {
   return (
     <div classNameName="portfolio">
       <div className="card-group">
   <div className="card">
-    <img className="card-img-top" src="https://external-preview.redd.it/_PQ_zQ__0EMI4iJTWhSVI_Ocul5ZW1I2G-RLYQsRdpM.jpg?auto=webp&s=6f1659fe3a3836192e8a608843b01890e17fc327" alt="Card image cap"/>
+    <img className="card-img-top" src={walkimg}alt="Card image cap"/>
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      <h5 className="card-title">When Should I Walk?</h5>
+      <p className="card-text">For this app, built with React, external APIs, Express, MySQL and Boostrap, I developed a sophisticated algorthim to tell people when to take their one-hour walk in lockdown. Feature extension by Andrea Nowe.</p>
+      
     </div>
   </div>
   <div className="card">
-    <img className="card-img-top" src="https://external-preview.redd.it/_PQ_zQ__0EMI4iJTWhSVI_Ocul5ZW1I2G-RLYQsRdpM.jpg?auto=webp&s=6f1659fe3a3836192e8a608843b01890e17fc327" alt="Card image cap"/>
+    <img className="card-img-top" src={workwithme} alt="Card image cap"/>
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      <h5 className="card-title">Work With Me</h5>
+      <p className="card-text">Full-stack app that allows users to find people to co-work with in their area. They can also offer their own space as a coworking space.</p>
+      
     </div>
   </div>
   <div className="card">
@@ -25,7 +26,7 @@ export default function Portfolio() {
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      
     </div>
   </div>
 </div>
