d9fe8bf0922eaa0539abb57742dcfa40e2ef255f
Maisie <maisieobb@googlemail.com>
2021-05-03 16:32:19 +0100

    Adding cards to portfolio page

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,17 +1,34 @@
-import React from 'react';
-// import portfolioimg from "Portfolio.PNG";
+import React from "react";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio">
-            <div className="carousel-item">
-            <img src="./Portfolio.PNG" alt="This is the portfolio image"/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>Hell here's h5</h5>
-                <p>This app is cool</p>
-            </div>
-            </div>
-
-        </div>
-    )
+  return (
+    <div classNameName="portfolio">
+      <div className="card-group">
+  <div className="card">
+    <img className="card-img-top" src="https://external-preview.redd.it/_PQ_zQ__0EMI4iJTWhSVI_Ocul5ZW1I2G-RLYQsRdpM.jpg?auto=webp&s=6f1659fe3a3836192e8a608843b01890e17fc327" alt="Card image cap"/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img className="card-img-top" src="https://external-preview.redd.it/_PQ_zQ__0EMI4iJTWhSVI_Ocul5ZW1I2G-RLYQsRdpM.jpg?auto=webp&s=6f1659fe3a3836192e8a608843b01890e17fc327" alt="Card image cap"/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img className="card-img-top" src="https://external-preview.redd.it/_PQ_zQ__0EMI4iJTWhSVI_Ocul5ZW1I2G-RLYQsRdpM.jpg?auto=webp&s=6f1659fe3a3836192e8a608843b01890e17fc327" alt="Card image cap"/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+    </div>
+  );
 }
