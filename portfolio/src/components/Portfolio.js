a7af0b39a0e2a7a24a416a37075f640b9fe05c0b
Maisie <maisieobb@googlemail.com>
2021-05-03 16:22:38 +0100

    add image file

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+// import portfolioimg from "Portfolio.PNG";
 
 export default function Portfolio() {
     return (
         <div className="portfolio">
-            portfolio
+            <div className="carousel-item">
+            <img src="./Portfolio.PNG" alt="This is the portfolio image"/>
+            <div className="carousel-caption d-none d-md-block">
+                <h5>Hell here's h5</h5>
+                <p>This app is cool</p>
+            </div>
+            </div>
+
         </div>
     )
 }
