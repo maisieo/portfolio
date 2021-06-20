62f77e0c01446f7566d5a5896b399613e8d30739
Maisie <maisieobb@googlemail.com>
2021-06-20 14:11:06 +0100

    remove header and nav from landing page, start array of projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,8 +6,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div>
-    <Header/>
-    <Navbar />
+   
     <Routes />
     </div>
   );
--- portfolio/src/components/Routes.js
+++ portfolio/src/components/Routes.js
@@ -1,40 +1,47 @@
-import { React} from "react";
+import { React } from "react";
 import { Route, Switch, Router } from "react-router-dom";
 // import HomeView from "./HomeView";
-import AboutMe from "./AboutMe"
-import Portfolio from "./Portfolio"
-import Technologies from "./Technologies"
-import Contact from "./Contact.js"
-import Landing from "./Landing"
+import AboutMe from "./AboutMe";
+import Portfolio from "./Portfolio";
+import Technologies from "./Technologies";
+import Contact from "./Contact.js";
+import Landing from "./Landing";
+import Header from "./Header";
+import Navbar from "./Navbar"
 
 // import AuthenticatedRoute from "./AuthenticatedRoute";
 
 function Routes() {
   return (
-   
-    <Switch> 
-       <Route path="/" exact>
-        <Landing />
+    <Switch>
+      <Route path="/" exact>
+       <Landing />
       </Route>
 
       <Route path="/about-me" exact>
+        <Header />
+        <Navbar />
         <AboutMe />
       </Route>
 
       <Route exact path="/portfolio">
-      <Portfolio/>
+        <Header />
+        <Navbar />
+        <Portfolio />
       </Route>
 
       <Route exact path="/technologies">
-      <Technologies/>
+        <Header />
+        <Navbar />
+        <Technologies />
       </Route>
 
       <Route exact path="/contact">
-      <Contact/>
+        <Header />
+        <Navbar />
+        <Contact />
       </Route>
-      </Switch>
-      
-    
+    </Switch>
   );
 }
 
